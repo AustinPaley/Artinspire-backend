3f444c5b9d17f6eacb1f106418f992c7e9fc68f7
AustinPaley <austinpaley32@gmail.com>
2018-08-21 13:58:03 -0400

    Added Frontend

--- Artinspire/config/routes.rb
+++ Artinspire/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :words, only [:index]
-      resources :arttypes, only [:index]
+      resources :words, only: [:index]
+      resources :arttypes, only: [:index]
     end
   end
 end
